4c150315fc9bacc077ee65619335658c4642ea7b
sahil3vedi <sahiltrivediw@gmail.com>
2021-03-16 12:00:58 +0530

    added Admin dashboard & login

---
+++ src/dashboard/inventory.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import { Button } from 'antd'
+
+class Inventory extends Component{
+    render(){
+        return (
+            <div>
+                <p className="workspace-title">Inventory</p>
+            </div>
+        )
+    }
+}
+
+export default Inventory
---
+++ src/dashboard/suppliers.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import { Button } from 'antd'
+
+class Suppliers extends Component{
+    render(){
+        return (
+            <div>
+                <p className="workspace-title">Suppliers</p>
+            </div>
+        )
+    }
+}
+
+export default Suppliers
